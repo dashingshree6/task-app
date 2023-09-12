7187ff55e7c17f1f3fd2f333eab1fb58a2fadb37
shreyas <83569207+dashingshree6@users.noreply.github.com>
2023-09-12 18:59:23 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+// app.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://dashingshree6:kb85YfsvE5xigATA@cluster0.zisswne.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+    console.log("DB Connected");
+});
+
+
+// Use CORS middleware to handle CORS
+app.use(cors());
+// Middleware
+app.use(express.json());
+
+// Define your routes here
+const userRoutes = require('./routes/user');
+const taskRoutes = require('./routes/task');
+
+app.use('/user', userRoutes);
+app.use('/tasks', taskRoutes);
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
