14e623bb94f459ca2339aaf641270e1310435d85
dashingshree6 <dashingshree6@gmail.com>
2023-09-14 15:17:00 +0530

    Update middleware.js

--- controller/middleware.js
+++ controller/middleware.js
@@ -2,34 +2,30 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.verifyToken = (req, res, next) => {
+  console.log('.................',req.headers.authorization)
   res.setHeader("Content-Type", "application/json");
   if (
     req.headers &&
     req.headers.authorization &&
     req.headers.authorization.split(" ")[0] === "Bearer"
   ) {
-    console.log(jwt.verify(
-        req.headers.authorization.split(" ")[1],
-        'TASKAPP123'))
+
     jwt.verify(
       req.headers.authorization.split(" ")[1],
       'TASKAPP123',
       async function (err, decode) {
         if (err) req.user = undefined;
-        console.log("decode", decode, decode.userId);
          let user = await User.findOne({_id: decode?.userId}).exec()
             req.user = user;
             console.log(req.user, "User");
             userreq = user;
             next();
-        
-    
       }
     );
   } else {
     // req.user = undefined;
     if (!req.user) {
-      return res.status(404).json({ message: "please provide user token" });
+      return res.status(404).json({ message: "Please provide user token" });
     }
 
     next();
